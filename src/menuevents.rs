38c2fde222a7a24f6f4298eb7596832050836f35
Pyś Pysio <jerzy.gorski.art@gmail.com>
2020-11-17 17:47:11 +0100

    missing files

---
+++ src/menuevents.rs
@@ -0,0 +1,58 @@
+use amethyst::{
+    derive::SystemDesc,
+    ecs::prelude::{System, SystemData},
+    ecs::{Read, WriteStorage},
+    shrev::{EventChannel, ReaderId},
+    ui::{UiEvent, UiEventType, UiText, UiTransform,},
+    utils::application_root_dir,
+};
+use crate::mainmenu::TEMP_LOGO;
+
+#[derive(SystemDesc)]
+#[system_desc(name(UiEventHandlerSystemDesc))]
+pub struct UiEventHandlerSystem {
+    #[system_desc(event_channel_reader)]
+    reader_id: ReaderId<UiEvent>,
+    
+}
+
+impl UiEventHandlerSystem {
+    pub fn new(reader_id: ReaderId<UiEvent>) -> Self {
+        Self { reader_id }
+    }
+}
+
+impl<'s> System<'s> for UiEventHandlerSystem {
+    type SystemData = (
+        Read<'s, EventChannel<UiEvent>>,
+        WriteStorage<'s, UiTransform>,
+        WriteStorage<'s, UiText>,
+    );
+
+    fn run(&mut self, (events, mut transforms, mut texts): Self::SystemData) {
+        for event in events.read(&mut self.reader_id) {
+            let button_text = texts.get_mut(event.target).unwrap();
+            let trans = transforms.get_mut(event.target).unwrap();
+            if trans.id != TEMP_LOGO {
+                match event.event_type {
+                    UiEventType::HoverStart => { 
+                        button_text.color[3] = 1.0; 
+                    },
+                    UiEventType::HoverStop  => { 
+                        button_text.color[3] = 0.5; 
+                    },
+                    UiEventType::ClickStart  => { 
+                        button_text.color[3] = 0.1; 
+                        trans.local_y -= 2.0;
+                    },
+                    UiEventType::ClickStop  => { 
+                        button_text.color[3] = 1.0; 
+                        trans.local_y += 2.0;
+                    },
+                    _ => {},
+                }
+            }
+        }
+        
+    }
+}
