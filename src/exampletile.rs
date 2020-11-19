b3f0b32f9766cccb8345fc1e86e81a6d0aedbf35
Pyś Pysio <jerzy.gorski.art@gmail.com>
2020-11-19 13:01:28 +0100

    new tileset

--- src/exampletile.rs
+++ src/exampletile.rs
@@ -3,11 +3,15 @@ use amethyst::{
     prelude::*,
     tiles::Tile,
  };
+// use rand::Rng;
 
 #[derive(Default, Clone)]
 pub struct ExampleTile;
 impl Tile for ExampleTile {
-    fn sprite(&self, _: Point3<u32>, _: &World) -> Option<usize> {
-        Some(1)
+    fn sprite(&self, _point: Point3<u32>, _: &World) -> Option<usize> {
+        //let mut rng = rand::thread_rng();
+        //let tile_value = rng.gen_range(0, 255);
+        //println!("{}",tile_value);
+        Some(40)
     }
 }
